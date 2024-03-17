00ae49ff13dda2a0c752bedf31fab63537ee0b64
Azazel111 <azamsaf21@gmail.com>
2024-03-17 14:35:44 +0300

    Azamat HomePage Done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,68 @@
 console.log('hello');
+// SLIDER IN FIRSTBANNER
+if (document.querySelector('.first-banner-swiper')) {
+    const swiper = new Swiper('.first-banner-swiper', {
 
-const swiper = new Swiper('.swiper', {
+        // If we need pagination
+        pagination: {
+            el: '.first-banner-slider-pagination',
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.first-banner-slider__next',
+            prevEl: '.first-banner-slider__prev',
+        },
+        allowSwipeToPrev: false,
+        allowSwipeToNext: false,
+    });
+}
 
-    // If we need pagination
-    pagination: {
-        el: '.first-banner-slider-pagination',
-    },
-    // Navigation arrows
-    navigation: {
-        nextEl: '.first-banner-slider__next',
-        prevEl: '.first-banner-slider__prev',
-    },
-    allowSwipeToPrev: false,
-    allowSwipeToNext: false,
-});
+// SLIDER UTP
+if (document.querySelector('.utp')) {
+    const utp = new Swiper('.utp', {
+        loop: true,
+        autoplay: {
+            dalay: 2000,
+        },
+        // freeMode: true,
+        slidesPerView: 4,
+    });
+}
+
+if(document.querySelector('.video-content')) {
+    const video = document.querySelector('.video-content');
+    const videoPlay = document.querySelector('.video-play');
+    let isPlay = false;
+    videoPlay.addEventListener('click', function() {
+        if(isPlay == false) {
+            video.play();
+            // video.setAttribute('controls');
+            video.setAttribute('controls', 'controls');
+            isPlay = true;
+            videoPlay.children[0].style.display = 'none';
+        } else {
+            video.pause();
+            video.removeAttribute('controls');
+            videoPlay.children[0].style.display = 'block';
+            isPlay = false;
+        }
+    });
+}
+
+// SLIDER NEWS IN HOME PAGE
+if (document.querySelector('.news-slider')) {
+    const swiper = new Swiper('.news-slider', {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 24,
+        // pagination: {
+        //     el: '.first-banner-slider-pagination',
+        // },
+        // navigation: {
+        //     nextEl: '.first-banner-slider__next',
+        //     prevEl: '.first-banner-slider__prev',
+        // },
+        // allowSwipeToPrev: false,
+        // allowSwipeToNext: false,
+    });
+}
